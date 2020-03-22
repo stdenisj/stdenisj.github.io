ff0fd4aca209a8756652359f214e62aba665dd54
stdenisj <stdenisjames@gmail.com>
2020-03-22 00:27:04 -0400

    imported projectcarousel into projects component, removed extra closing brace

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import { container } from 'react-bootstrap'
+import ProjectCarousel from './ProjectCarousel'
+import { Container } from 'react-bootstrap'
 
 export default function Projects() {
     const projectStyle = {
-
+        height: '100vh',
+        justifyContent: 'center',
+        padding: '40vh'
     }
+
     return (
         <Container fluid>
-            
+            <h1> Projects </h1>
+
+            <ProjectCarousel />
         </Container>
     )
 }
