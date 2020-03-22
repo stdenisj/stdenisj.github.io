060bec2f64918e3a8df0e2ce259a269539ec078d
stdenisj <stdenisjames@gmail.com>
2020-03-21 23:43:22 -0400

    created about me component

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+export default function AboutMe() {
+    const style = {
+        height: '100%',
+        justifyContent: 'center'
+    }
+    return (
+        <Container fluid>
+            <h1> About Me </h1>
+        </Container>
+    )
+}
