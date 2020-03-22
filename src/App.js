c01548a9cc5dd550b2b93722c1bd7e5d3d4f7e6c
stdenisj <stdenisjames@gmail.com>
2020-03-21 23:44:33 -0400

    imported about me into app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navigation from './components/Navigation'
+import AboutMe from './components/AboutMe'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <AboutMe />
     </div>
   );
 }
