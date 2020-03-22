1c9e51a430b49c5d2427164d253b0efc066b53a5
stdenisj <stdenisjames@gmail.com>
2020-03-22 01:24:46 -0400

    updated styling

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,8 +4,6 @@ import { Container } from 'react-bootstrap'
 export default function AboutMe() {
     const componentStyle = {
         height: '100vh',
-        justifyContent: 'center',
-        padding: '40vh'
     }
     return (
         <Container style={ componentStyle }fluid>
