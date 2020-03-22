5dd32b5c1754a77739f8f34580f028dde2d23b72
stdenisj <stdenisjames@gmail.com>
2020-03-21 23:56:32 -0400

    created p tag and filler for about me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,6 +9,12 @@ export default function AboutMe() {
     return (
         <Container style={ componentStyle }fluid>
             <h1> About Me </h1>
+
+            <p> 
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Fuga, sapiente similique! Repellat cum ad ipsa! Harum, similique ab. 
+                Illo animi cumque beatae neque cum quas est molestias incidunt quisquam sit.
+            </p>
         </Container>
     )
 }
