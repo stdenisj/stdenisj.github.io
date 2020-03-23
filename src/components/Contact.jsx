9c541508134739e924e9d5939c20b2b8caa326f8
stdenisj <stdenisjames@gmail.com>
2020-03-22 22:41:24 -0400

    created contact page

---
+++ src/components/Contact.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Container, Col, Row } from 'react-bootstrap'
+import GitHub from './FontAwesomeComponents/GitHub'
+import LinkedIn from './FontAwesomeComponents/LinkedIn'
+
+export default function Contact() {
+    const contactStyle = {
+        height: '100vh',
+        padding: '50vh 0vh'
+    }
+    return (
+        <Container style={ contactStyle } fluid>
+            <Row>
+                <Col>
+                    <LinkedIn />
+                </Col>
+                <Col>
+                    <GitHub />
+                </Col>
+                <Col>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
